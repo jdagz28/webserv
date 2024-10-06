6d4349a6bf8ffb8574c3d2c6530712e392687aa0
jdagz28 <joshdagoy28@gmail.com>
2024-10-07 01:21:50 +0200

    Location block checks - parseIndex, parseAutoindex, parseRedirect

--- srcs/config/parseLocationBlock.cpp
+++ srcs/config/parseLocationBlock.cpp
@@ -6,7 +6,7 @@
 /*   By: jdagoy <jdagoy@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 14:50:02 by jdagoy            #+#    #+#             */
-/*   Updated: 2024/10/04 15:29:10 by jdagoy           ###   ########.fr       */
+/*   Updated: 2024/10/07 01:19:54 by jdagoy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,67 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <algorithm>
 
+
+void Config::parseRedirect(const std::string &value, LocationConfig &locationConfig)
+{
+    std::vector<std::string> validRedirects{"301", "302", "303", "307", "308"};
+    
+    std::vector<std::string> values = splitBySpaces(value);
+    if (values != 2)
+    {
+        _error = std::string("invalid number of arguments in ") + GREEN + "\"return\"" + RESET + " directive";
+        throw configException(_error, _configPath, _parsedLine);
+    }
+    if (std::find(validRedirects.begin(), validRedirects.end(), values[0]) == validRedirects.end())
+    {
+        _error = std::string("invalid value in ") + GREEN + "\"return\"" + RESET + " directive";
+        throw configException(_error, _configPath, _parsedLine);
+    }
+    locationConfig.setDirective("return", value);
+}
+
+void Config::parseAutoindex(const std::string &value, LocationConfig &locationConfig)
+{
+    if (value != "on" && value != "off")
+    {
+        _error = std::string("invalid value in ") + GREEN + "\"autoindex\"" + RESET + " directive";
+        throw configException(_error, _configPath, _parsedLine);
+    }
+    locationConfig.setDirective("autoindex", value);
+}
+
+void Config::parseIndex(const std::string &value, LocationConfig &locationConfig)
+{
+    for (size_t i = 0; i < value.size(); i++)
+    {
+        if (!std::isalnum(value[i]) && value[i] != '.')
+        {
+            _error = std::string("invalid value in ") + GREEN + "\"index\"" + RESET + " directive";
+            throw configException(_error, _configPath, _parsedLine);
+        }
+    }
+    locationConfig.setDirective("index", value);
+}
 
 
 void Config::parseRoot(const std::string &value, LocationConfig &locationConfig)
 {
-
+    for (size_t i = 0; i < value.size(); i++)
+    {
+        if (!std::isalnum(value[i]) && value[i] != '/')
+        {
+            _error = std::string("invalid value in ") + GREEN + "\"root\"" + RESET + " directive";
+            throw configException(_error, _configPath, _parsedLine);
+        }
+    }
+    locationConfig.setDirective("root", value);
 }
 
 void Config::checkValueNum(const std::string &token, const std::string &value)
 {
-    if (token == "return" || token == "limit_except")
+    if (token == "return" || token == "limit_except" || token == "types")
         return ;
     std::vector<std::string> values = splitBySpaces(value);
     if (value.empty() || values.size() != 1)
@@ -37,7 +87,7 @@ void Config::checkValueNum(const std::string &token, const std::string &value)
 
 void Config::validLocationDirective(const std::string &token)
 {
-    if (token == "root" || token == "index" || token == "autoindex" || token == "return" || token == "limit_except")
+    if (token == "root" || token == "index" || token == "autoindex" || token == "return" || token == "limit_except" || token == "types")
         return (true);
     return (false);   
 }
@@ -68,6 +118,8 @@ void Config::parseLocationDirective(const std::string &token, std::istringstream
             parseRedirect(value, LocationConfig)
         else if (token == "limit_except")
             parseLimitExcept(infile, LocationConfig);
+        else if (token == "types")
+            parseTypes(value, LocationConfig);
     }
     else
     {
