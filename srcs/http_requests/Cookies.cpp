8c6663d38f2840deb3cfdbf6b85b30effef42339
jdagz28 <joshdagoy28@gmail.com>
2025-01-31 11:32:09 +0100

    stash from old-cookies

---
+++ srcs/http_requests/Cookies.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Cookies.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdagoy <jdagoy@student.s19.be>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/02 08:50:11 by jdagoy            #+#    #+#             */
+/*   Updated: 2025/01/31 11:29:15 by jdagoy           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "HttpRequest.hpp"
+#include "webserv.hpp"
+#include <iterator>
+
+const std::string HttpRequest::printCookies() const
+{
+    std::string cookies;
+    if (_cookies.empty())
+        return (std::string()); 
+    else
+    {        
+        std::map<std::string, std::string>::const_iterator it;
+        std::map<std::string, std::string>::const_iterator lastEntry;
+        lastEntry = --_cookies.end();
+        for (it = _cookies.begin(); it != _cookies.end(); it++)
+        {
+            cookies += it->first + "=" + it->second;
+            if (it != lastEntry)
+                cookies += "; ";
+        }
+    }
+    return (cookies);
+}
+
+const std::map<std::string, std::string> &HttpRequest::getCookies() const
+{
+    return (_cookies);
+}
