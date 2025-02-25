71092e16860c902c64ddb9165ceb3122cc6deca9
Romain Van der vennet <romvan-d@fu-c4-p3.s19.be>
2025-02-25 17:12:19 +0100

    adding cgi

--- srcs/cgi/Cgi.cpp
+++ srcs/cgi/Cgi.cpp
@@ -6,7 +6,7 @@
 /*   By: romvan-d <romvan-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 19:52:54 by romvan-d          #+#    #+#             */
-/*   Updated: 2025/02/25 15:25:03 by romvan-d         ###   ########.fr       */
+/*   Updated: 2025/02/25 17:09:14 by romvan-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,4 +37,20 @@ Cgi:Cgi(HttpRequestLine & request, std::string path, std::string uploadDir)
 		this->whichMethod = GET;
 		this->data = request.
 	}
+}
+
+Cgi::Cgi(const Cgi &other) : env(other.env)
+{
+
+}
+
+Cgi::~Cgi()
+{
+
+}
+
+
+const char *Cgi::CgiError::what() const throw()
+{
+        return("Cgi::CgiError : Interal  Error Cgi.");
 }
