74f5b379f47b02ddd0be5ca7372812aafc6e5302
Romain Van der vennet <romvan-d@fu-c4-p3.s19.be>
2025-03-03 18:27:48 +0100

    updating cgi.cpp

--- includes/Cgi.hpp
+++ includes/Cgi.hpp
@@ -6,14 +6,14 @@
 /*   By: romvan-d <romvan-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 17:08:50 by romvan-d          #+#    #+#             */
-/*   Updated: 2025/02/25 15:17:02 by romvan-d         ###   ########.fr       */
+/*   Updated: 2025/03/03 18:17:26 by romvan-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CGI_HPP
 # define CGI_HPP
 
-#include "Httprequest.hpp"
+#include "HttpRequest.hpp"
 #include "LocationConfig.hpp"
 #include <exception>
 #include <map>
@@ -25,12 +25,12 @@ class Cgi
 	public :
 	
 		Cgi();
-		Cgi(HttpRequestLine &request, std::string path, std::string uploadDir);
+		Cgi(HttpRequestLine &requestLine, HttpRequest & request, std::string path, std::string uploadDir);
         Cgi(const Cgi &other);
 		~Cgi();
 
 		Cgi &operator=(const Cgi &other);
-		std::string run();
+		std::string runCgi();
 
 		class CgiError : public std::exception
 		{
@@ -46,8 +46,10 @@ class Cgi
 		std::string							path;
 
 
-		char ** convertEnv(std::map<std::string, std::string>);
-		char ** convertArgs(std::vector<std::string>);
+		char ** convertEnv(std::map<std::string, std::string> env);
+		char ** convertArgs(std::vector<std::string> args);
 		std::string readPipe(int pipeRead);
 
 };
+
+#endif
